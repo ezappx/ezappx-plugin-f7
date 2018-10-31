70ecbca9b76b268b0deb7e6b8b8d780c47f8d031
ingbyr <ingbyr@outlook.com>
2018-10-31 16:53:05 +0800

    update plugin at Wed Oct 31 16:53:00     2018

--- src/traits.js
+++ src/traits.js
@@ -94,7 +94,7 @@ export default function (editor, opt = {}) {
 
   trm.addType('createPage', {
     events: {
-      'keyup': 'onChange',
+      'focusout': 'onChange',
     },
 
     onValueChange: function () {
